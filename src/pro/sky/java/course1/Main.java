2e00b7130c7efcc41e78acd1ed7e09c0a69eab05
L <EBogatyrevaPK@yandex.ru>
2022-02-05 20:47:51 +1200

    Здравствуйте! надеюсь все сделала верно!)

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -3,6 +3,32 @@ package pro.sky.java.course1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+        // Домашнее задание 1.4.
+        // Задание 1.
+        int i = 1;
+        while (i < 11) {System.out.print(i); i++; System.out.print(" ");}
+        System.out.println();
+        for (int j = 10; j > 0; j--) {System.out.print(j); System.out.print(" ");}
+
+        // Задание 2.
+        System.out.println(" ");
+        int dataOfFriday = 7; //условно первая пятница месяца
+        int numberOfWeek = 1;//Порядковый номер недели в месяце
+
+        while (numberOfWeek < 5) {
+            System.out.println("Сегодня пятница, " + dataOfFriday + "-е число. Необходимо подготовить отчет.");
+            dataOfFriday += 7;
+            numberOfWeek++;}
+
+        // Задание 3.
+        System.out.println(" ");
+        int lastYear = 1822;
+        int futureYear = 2122;
+        int j = lastYear;
+
+        while (j < futureYear) {
+            if (j % 79 == 0) {System.out.println(j);
+            } j++;
+        }}
 }
+
